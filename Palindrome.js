6c6613aa6a63d304d58952a64193884aed42e710
Jose Antonio <99999793+josant12@users.noreply.github.com>
2022-03-04 00:31:42 -0600

    Update Palindrome.js

--- Palindrome.js
+++ Palindrome.js
@@ -1,15 +1,25 @@
 function palindrome(str) {
-  
-  let newStr = str.toLowerCase().replace(/\W+|_/g,"");
-  let reverseStr = newStr.split("").reverse().join("");
-
-  if( newStr === reverseStr){
+  let regex = /[\W_]/g
+  console.log(str);
+  str = str.toLowerCase();
+  console.log(str);
+  str = str.replace(regex, "");
+  console.log(str);
+  let str2 =str.split("").reverse().join("");
+  console.log(str2);
+  if(str===str2){
     return true;
-  } else {
+  }else{
     return false;
   }
-
+  
 }
 
-
-console.log(palindrome("A man, a plan, a canal. Panama"));
+function palindromeChecker(){
+  let input = document.getElementById("inputPalindromo");
+  let valinput = input.value;
+  let check = palindrome(valinput);
+  let result = document.getElementById("result");
+  if(check) result.innerText = "Es un palindromo";
+  else result.innerText = "No es un palindromo";
+}
